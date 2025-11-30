cc6b423027739513125966e349dbe32df8c80fc5
Prem Kumar <premkr017@gmail.com>
2025-12-01 03:42:15 +0530

    add function examples and explanations to function

---
+++ function.py
@@ -0,0 +1,60 @@
+# 1. Simple Function – Namaste Bolne Ka
+# def say_hello():
+#     print("Namaste! Kaise ho?")
+    
+# say_hello()
+
+# 2. Function With Parameter – Naam Print Karna
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Ritik")
+# greet("Rahul")
+# greet("Anjali")
+
+# 3. Do Numbers Ka Addition
+# def add(a, b):
+#     return a + b
+
+# result = add(10, 20)
+# print("Total =", result)
+
+# 4. Age Check Function
+
+# def check_age(age):
+#     if age >= 18:
+#         print("Aap vote de sakte ho.")
+#         print("kyonki aapki age 18+ se jyada hai.")
+#     else:
+#         print("Aap vote nahi de sakte.")
+#         print("kyonki aapki umr 18 saal sal se kam hai.")
+
+# check_age(17)
+# check_age(20)
+
+# 5. Square Nikalne Ka Function
+
+# def square(n):
+#     return n * n
+
+# print(square(5))
+# print(square(9))
+
+# 6. List Ka Total Nikalna
+# def total(numbers):
+#     sum = 0
+#     for num in numbers:
+#         sum += num
+#     return sum
+
+# print(total([10, 20, 30, 40]))
+# print(total([5, 15, 25]))
+
+# 7. Function With Default Value
+# def greet(name="Friend"):
+#     print("Hello", name)
+
+# greet()
+# greet("Ritik")
+# greet("Anjali")
---
+++ main.py
@@ -0,0 +1,5 @@
+# ye page h jab koi code kisi page pe kam nal kare to wo page 404 error dikhata hai.ai jata hai. Iska matlab hai ki jo resource aap dhoondh rahe hain wo server pe nahi mil raha hai. Ye error tab hota hai jab URL galat ho, resource delete ho gaya ho, ya fir server pe koi issue ho. 404 error ko handle karne ke liye aap apne web application me custom 404 error page bana sakte hain jo users ko better experience de sake. Iske alawa, aap apne website ke links ko regularly check kar sakte hain taaki broken links na ho. Agar aap kisi website ka developer hain, to ensure karein ki sabhi resources sahi tarah se server pe host kiye hain aur URLs sahi hain. 404 error ko samajhna aur usse effectively handle karna web development ka ek important hissa hai.
+# main.py
+#  es liye h ki jab koi bhi code kisi page pe kam nahi karta h to us code ko es page pe likh ke run kar sakte hain. 404 error ka matlab hota hai "Not Found", yani ki jo resource aap dhoondh rahe hain wo server pe nahi mil raha hai
+
+
